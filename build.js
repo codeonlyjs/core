eca2e8c6e5df727e75c7d76ea41cf774e903e95c
Brad Robinson <brad@toptensoftware.com>
2024-11-01 09:18:45 +1100

    0.0.2

--- build.js
+++ build.js
@@ -1,7 +1,12 @@
+import {syncProcessCwd} from 'zx'
+syncProcessCwd();
+
+/*
 if (await $`git diff --exit-code && git diff --cached --exit-code && git push -n`.exitCode != 0)
     throw new Error("Uncommitted or unpushed changes");
 if (await $`(cd ../dist/ && git diff --exit-code && git diff --cached --exit-code && git push -n)`.exitCode != 0)
     throw new Error("Uncommitted or unpushed changes");
+*/
 
 // Run tests
 console.log("Running tests...");
@@ -9,28 +14,27 @@ if (await $`npm run test`.exitCode != 0)
     throw new Error("Unit tests failed");
 
 // Update version
-await $`npm version patch`
+//await $`npm version patch --no-git-tag-version`
 
 // Get the package version
 let pkg = await $`cat package.json`.json();
 console.log(`Package Version: ${pkg.version}`);
 
 // Run rollup
-await $`npm run rollup`
+//await $`npm run rollup`
 
 // Tag and commit both repos
-await $`(cd ../dist/ && ${git_tag_and_commit()})`
-await $`${git_tag_and_commit()}`
+await git_tag_and_commit();
+cd("../dist/")
+await git_tag_and_commit();
 
 console.log("Build Completed Successfully");
 
 async function git_tag_and_commit()
 {
-    return [
-        `git add .`
-        `git commit -m "${pkg.version}" --allow-empty`
-        `git tag -f "${pkg.version}"`
-        `git push --quiet`
-        `git push -f --tags --quiet`
-    ].join(" && ");
+    await $`git add .`
+    await $`git commit -m "${pkg.version}" --allow-empty`
+    await $`git tag -f "${pkg.version}"`
+    await $`git push --quiet`
+    await $`git push -f --tags --quiet`
 }
