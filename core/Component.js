9b44fbea893c9412d34c5d742c517553b3e400dc
Brad Robinson <brad@toptensoftware.com>
2024-11-16 18:11:39 +1100

    Remove Component.cleanup tweak Component.listen

--- core/Component.js
+++ core/Component.js
@@ -214,6 +214,10 @@ export class Component extends EventTarget
     #listeners;
     listen(target, event, handler)
     {
+        if (!target || !event)
+            return;
+        if (!handler)
+            handler = this.invalidate;
         if (!this.#listeners)
             this.#listeners = [];
         this.#listeners.push({
@@ -225,7 +229,7 @@ export class Component extends EventTarget
 
     unlisten(target, event, handler)
     {
-        if (!this.#listeners)
+        if (!target || !event || !this.#listeners)
             return;
         let index = this.#listeners.findIndex(x =>
             x.target == target &&
@@ -239,24 +243,6 @@ export class Component extends EventTarget
                 target.removeEventListener(event, handler);
         }
     }
-
-    setInterval()
-    {
-        let interval = setInterval(...arguments);
-        this.cleanup(() => clearInterval(interval));
-        return interval;
-    }
-
-    cleanup(cb)
-    {
-        if (!this.#mounted)
-            throw new Error("Invalid when not mounted");
-        if (!this.#cleanups)
-            this.#cleanups = [];
-        this.#cleanups.push(cb);
-    }
-
-    #cleanups;
 
     get mounted()
     {
@@ -285,16 +271,6 @@ export class Component extends EventTarget
         // Remove event listeners
         if (!mounted && this.#listeners)
             this.#listeners.forEach(x => x.target.removeEventListener(x.event, x.handler));
-
-        // Invoke all clean up callbacks
-        if (!mounted)
-        {
-            if (this.#cleanups)
-            {
-                this.#cleanups.forEach(x => x());
-                this.#cleanups = [];
-            }
-        }
     }
 
     mount(el)
