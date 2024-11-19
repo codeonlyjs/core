ef22f7555a52db181a299448815b208367552409
Brad Robinson <brad@toptensoftware.com>
2024-11-19 15:33:57 +1100

    Fix css function

--- core/Style.js
+++ core/Style.js
@@ -42,10 +42,10 @@ function mountStyles()
 export function css(strings, values)
 {
     let r = "";
-    for (let i=0; i<strings.length; i++)
+    for (let i=0; i<strings.length - 1; i++)
     {
         r += strings[i];
-        r += values[i];
+        r += arguments[i + 1];
     }
     r += strings[strings.length - 1];
     Style.declare(r);
