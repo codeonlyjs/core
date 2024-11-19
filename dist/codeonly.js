8c55490484ec47ace7191229133b7d49e9fa4e35
Brad Robinson <brad@toptensoftware.com>
2024-11-19 15:34:11 +1100

    0.0.55

--- dist/codeonly.js
+++ dist/codeonly.js
@@ -132,10 +132,10 @@ function mountStyles()
 function css(strings, values)
 {
     let r = "";
-    for (let i=0; i<strings.length; i++)
+    for (let i=0; i<strings.length - 1; i++)
     {
         r += strings[i];
-        r += values[i];
+        r += arguments[i + 1];
     }
     r += strings[strings.length - 1];
     Style.declare(r);
