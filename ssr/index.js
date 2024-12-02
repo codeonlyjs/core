cd9bf6dd4919aea2516c776c8b638d04bc031576
Brad Robinson <brad@toptensoftware.com>
2024-12-02 15:58:59 +1100

    Export SSRWorker

--- ssr/index.js
+++ ssr/index.js
@@ -2,6 +2,7 @@ export * from "../spa/index.js";
 
 export * from "./SSREnvironment.js";
 export * from "./TemplateCompilerSSR.js";
+export * from "./SSRWorker.js";
 export * from "./SSRWorkerThread.js";
 export * from "./generateStatic.js";
 export * from "./viteGenerateStatic.js";
