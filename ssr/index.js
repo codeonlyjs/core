c071c2feb9ffd42308c281f1150ef139ca8f10dc
Brad Robinson <brad@toptensoftware.com>
2024-12-04 15:32:07 +1100

    Fixed missing export

--- ssr/index.js
+++ ssr/index.js
@@ -1,7 +1,6 @@
 export * from "../spa/index.js";
 
 export * from "./SSREnvironment.js";
-export * from "./TemplateCompilerSSR.js";
 export * from "./SSRWorker.js";
 export * from "./SSRWorkerThread.js";
 export * from "./generateStatic.js";
